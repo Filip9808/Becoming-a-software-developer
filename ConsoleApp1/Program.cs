0e57bb9923aab7c5fa7871992080a2d2f3bdaeb8
Filip9808 <filipswiatek98@gmail.com>
2021-12-29 18:08:50 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double F, C;
+            Console.Write("Enter the temperature in Fahrenheit degrees (°F) ");
+            F = double.Parse(Console.ReadLine());
+            C = 5d / 9 * (F - 32);
+            Console.WriteLine($"Temperature in Celsius degrees: {C}°C");
+            Console.ReadKey();
         }
     }
 }
